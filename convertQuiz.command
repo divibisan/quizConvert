1b4a146ad8a0a18c46e7ec4f40d4323772766fbe
Daniel Berman <divibisan@gmail.com>
2017-07-12 08:44:27 -0400

    Accepts .docx, pipes converted file directly into quizConvert.py

--- convertQuiz.command
+++ convertQuiz.command
@@ -5,12 +5,13 @@ script_dir=$(dirname $0)
 echo $script_dir
 cd $script_dir
 
-for file in `find ./ -name "*.doc"`
-do                    
+mkdir converted_files
+
+# Get all .doc or .docx files in directory
+for file in `find ./ -name "*.doc*"`
+do
     echo $file
-    textutil -convert txt $file
-    file_text=${file%.doc}.txt
-    echo $file_text
-    python3 quizConvert.py $file_text
-    rm $file_text
+    # Convert to .txt using textutil, pass to quizConvert.py to process
+    #  and output .tsv files
+    textutil -stdout -convert txt $file | python3 ./quizConvert.py "$file"
 done
--- quizConvert.py
+++ quizConvert.py
@@ -1,23 +1,38 @@
 import re
 import sys
 
-inFile = sys.argv[1]
-outFile = re.match("(.+).txt$", inFile)
+# Check for correct command line arguments
+if len(sys.argv) != 2:
+    print("ERROR: missing filename as command line argument")
+    quit()
 
-if (inFile and outFile):
-    outFile = outFile[1] + ".tsv"
-    
-if (outFile != None):
-    w = open(outFile, 'w', encoding ='utf-16')
-    r = open(inFile, 'r', encoding='utf-8')
-    for line in r:
+# Use regex to extract filename without extension to use for output
+file_name = re.search(r"([^/]*)\.\w+$", sys.argv[1])
+
+# If successful, add extension to output file name
+if file_name:
+    file_name = file_name[1] + ".tsv"
+else:
+    print("ERROR: could not parse filename")
+    quit()
+
+print(file_name)
+
+# Open file in converted_files folder
+with open("converted_files/" + file_name, "w") as outFile:
+    for line in sys.stdin:
+        # Attempt to match line as either a question, answer option, or correct answer
         matchQuestion = re.search("Q. (.+)", line)
         matchOption = re.match("[ABCDabcd][).]\s+(.+)", line)
         matchAnswer = re.match("Answer:\s+(.)", line)
+        # Write matched line to file
+        #  questions and options get a tab
+        #  answers come last on the line and get a newline
         if matchQuestion:
-            w.write('"' + matchQuestion[1] + '"\t')
+            outFile.write('"' + matchQuestion[1] + '"\t')
         elif matchOption:
-            w.write('"' + matchOption[1] + '"\t')
+            outFile.write('"' + matchOption[1] + '"\t')
         elif matchAnswer:
-            w.write('"' + matchAnswer[1] + '"\n')
+            outFile.write('"' + matchAnswer[1] + '"\n')
+
 print("File Successfully Converted!\n")
